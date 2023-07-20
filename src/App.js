69d573cf2d2137d74401d3ab7e83cc62a90e9536
Soheil <soheilrajabali@gmail.com>
2023-07-20 14:55:38 -0400

    Navigation Bar Skeleton Created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import Navbar from "./Components/NavBar";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import "./Style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+          updateLoad(false);
+      }, 1200);
+
+      return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Robotics Website Template
-        </p>
-      </header>
-    </div>
+      <Router>
+          <div className="App" id={load ? "no-scroll" : "scroll"}>
+              <Navbar/>
+              {/*<Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/projects" element={<Projects/>}/>
+                  <Route path="/resume" element={<Resume/>}/>
+                  <Route path="/contact" element={<Contact/>}/>
+                  <Route path="/about" element={<About/>}/>
+              </Routes>*/}
+          </div>
+      </Router>
   );
 }
 
